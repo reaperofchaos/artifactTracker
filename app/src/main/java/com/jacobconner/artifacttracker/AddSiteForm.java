e516a408a1adf7bd30d8d593ae0aad8624240565
reaperofchaos <tokugawa111@gmail.com>
2022-12-02 21:55:36 -0500

    implement site form

--- app/src/main/java/com/jacobconner/artifacttracker/AddSiteForm.java
+++ app/src/main/java/com/jacobconner/artifacttracker/AddSiteForm.java
@@ -4,32 +4,97 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jacobconner.artifacttracker.databinding.ActivityAddSiteFormBinding;
+import com.jacobconner.artifacttracker.domain.Site;
+import com.jacobconner.artifacttracker.viewcontroller.SiteViewModel;
 
 public class AddSiteForm extends AppCompatActivity {
+    private SiteViewModel siteViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_site_form);
+        siteViewModel = new ViewModelProvider(this).get(SiteViewModel.class);
+
         initToolbar();
         initSaveButton();
     }
 
+    public static int tryParseInt(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException nfe) {
+            return 0;
+        }
+    }
+
+    public static float tryParseFloat(String value) {
+        try {
+            return Float.parseFloat(value);
+        } catch (NumberFormatException nfe) {
+            return 0.0F;
+        }
+    }
+
     private void initSaveButton() {
         Button saveBtn = findViewById(R.id.btnSaveSite);
-        TextView siteName = findViewById(R.id.txtSiteName);
-        TextView siteId = findViewById(R.id.txtSiteId);
-        TextView latitude = findViewById(R.id.txtX);
-        TextView longitude = findViewById(R.id.txtY);
+        EditText txtSiteName = findViewById(R.id.txtSiteName);
+        EditText txtSiteId = findViewById(R.id.txtSiteId);
+        EditText txtLatitude = findViewById(R.id.txtLatitude);
+        EditText txtLongitude = findViewById(R.id.txtLongitude);
+
+
 
         saveBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                String siteName = txtSiteName.getText().toString();
+                int siteId = tryParseInt(txtSiteId.getText().toString());
+                float latitude = tryParseFloat(txtLatitude.getText().toString());
+                float longitude = tryParseFloat(txtLongitude.getText().toString());
+                boolean nameIsEmpty = txtSiteName.getText().toString().trim().length() == 0;
+
+                Site newSite = new Site();
+                newSite.setId(siteId);
+                newSite.setSiteName(siteName);
+                newSite.setLatitude(latitude);
+                newSite.setLongitude(longitude);
+
+                if(!nameIsEmpty){
+                    //Create contact
+                    try{
+                        siteViewModel.insert(newSite);
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "The site," + siteName + " has been created",
+                                Toast.LENGTH_LONG).show();
+                    }catch(Exception exception){
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Error: The site was not added. \n" + exception,
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                    //clear the form
+                    txtSiteName.getText().clear();
+                    txtSiteId.getText().clear();
+                    txtLatitude.getText().clear();
+                    txtLongitude.getText().clear();
+                }else{
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Could not submit. Please type a name for your site.",
+                            Toast.LENGTH_LONG).show();
+                }
               //create a site and add to database
             }
         });
